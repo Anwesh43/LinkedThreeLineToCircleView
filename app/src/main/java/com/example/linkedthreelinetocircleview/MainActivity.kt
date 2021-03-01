2570f7e08b6b66d0ec18c8a8b306b97ee3bf5e4f
Anwesh43 <anweshthecool0@gmail.com>
2021-03-02 01:17:43 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedthreelinetocircleview/MainActivity.kt
+++ app/src/main/java/com/example/linkedthreelinetocircleview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedthreelinetocircleview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.threelinetocircleview.ThreeLineToCircleView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ThreeLineToCircleView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
