d13f0790d4a4b560efed8601857c43b099c711fb
Anwesh43 <anweshthecool0@gmail.com>
2021-03-02 00:54:51 +0530

    adding Canvas extension methods

--- threelinetocircleview/src/main/java/com/example/threelinetocircleview/ThreeLineToCircleView.kt
+++ threelinetocircleview/src/main/java/com/example/threelinetocircleview/ThreeLineToCircleView.kt
@@ -29,3 +29,38 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawThreeLineToCircle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = Math.min(w, h) / rFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        save()
+        translate(-w / 2 + (w / 2 - size) * sf2, -h / 2 + (h / 2 - size) * sf2)
+        drawLine(size * sf3, size * sf3, size * sf1, size * sf2, paint)
+        restore()
+        restore()
+    }
+    save()
+    translate(-w / 2 + (w / 2 - size) * sf2, 0f)
+    drawLine(size * sf3, size * sf3, size * sf1, size * sf1, paint)
+    drawCircle(0f, 0f, r * sf3, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawTLTCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawThreeLineToCircle(scale, w, h, paint)
+}
