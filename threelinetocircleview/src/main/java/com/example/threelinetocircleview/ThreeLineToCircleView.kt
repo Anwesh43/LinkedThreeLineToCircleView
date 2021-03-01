931fb0563cea4b0657c014beb51ae1a68688903d
Anwesh43 <anweshthecool0@gmail.com>
2021-03-02 00:31:56 +0530

    adding constants

--- threelinetocircleview/src/main/java/com/example/threelinetocircleview/ThreeLineToCircleView.kt
+++ threelinetocircleview/src/main/java/com/example/threelinetocircleview/ThreeLineToCircleView.kt
@@ -1,2 +1,26 @@
 package com.example.threelinetocircleview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#1abc9c",
+    "#8e44ad",
+    "#2ecc71",
+    "#2980b9",
+    "#e67e22"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val rFactor : Float = 3.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
